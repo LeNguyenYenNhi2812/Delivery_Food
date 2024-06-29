e521db9ad9b9c15cb692c4230d0d01b5adb9f252
Yen Nhi <nhi.lenguyenyennhi2004@hcmut.edu.vn>
2024-06-29 14:42:39 +0700

    first version

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here.</h2>
+        <p>Choose from a diverse menu what you like most. We provide a variety of dish from Euro to Asian that
+          will satisfy your hunger. We provide the best quality food that will make you come back for more.
+        </p>
+        <button>View menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/foodDisplay/foodDisplay.jsx
@@ -0,0 +1,16 @@
+// import React, { useContext } from 'react'
+// import './foodDisplay.css'
+// import { StoreContext } from '../../context/StoreContext'
+// const foodDisplay = (category) => {
+
+
+//     const {food_list}=useContext(StoreContext)
+
+//   return (
+//     <div className="food-display" id="food-display ">
+//       <h2>Top dishes near you. </h2>
+//     </div>
+//   )
+// }
+
+// export default foodDisplay
