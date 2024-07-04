d937bc0434fecb304bfa34521bd8afe3712eea4a
Yen Nhi <nhi.lenguyenyennhi2004@hcmut.edu.vn>
2024-07-04 11:16:59 +0700

    1:25

--- frontend/src/components/foodItem/foodItem.jsx
+++ frontend/src/components/foodItem/foodItem.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './foodItem.css'
 import { assets } from '../../assets/assets'
 const foodItem = ({id,name,price,description,image}) => {
+
+    const [itemCount,setItemCount]=useState(0)
+
+
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
