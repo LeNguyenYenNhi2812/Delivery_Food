ea9572b72c649f04d02a4df11c448856fcf93f57
Yen Nhi <nhi.lenguyenyennhi2004@hcmut.edu.vn>
2024-07-22 20:18:31 +0700

    22th7

--- frontend/src/components/foodItem/foodItem.jsx
+++ frontend/src/components/foodItem/foodItem.jsx
@@ -10,6 +10,14 @@ const foodItem = ({id,name,price,description,image}) => {
     <div className='food-item'>
       <div className="food-item-img-container">
         <img src={image} alt="" className="food-item-image" />
+        {!itemCount?
+        <img className="add" onClick={()=> setItemCount(prev=>prev+1) } src={assets.add_icon_white} alt=""  />:
+          <div className="food-item-counter">
+            <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+            <p> {itemCount} </p>
+            <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+          </div>
+        }
       </div> 
       <div className="food-item-info">
         <div className="food-item-name-rating">
