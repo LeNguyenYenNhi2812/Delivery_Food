200fa3e17175a9f5a2de524295ec5d0daa9f61cd
Yen Nhi <nhi.lenguyenyennhi2004@hcmut.edu.vn>
2024-09-16 00:13:52 +0700

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/home'
 import Cart from './pages/cart/cart'
 import PlaceOrder from './pages/placeOrder/placeOrder'
 import Footer from './components/footer/footer'
+import LoginPopup  from './components/loginPopup/loginPopup'
 const App = () => {
+  const [showLogin,setShowLogin]=useState(false);
   return (
+
     <>
+    {showLogin?<LoginPopup/>:<></>}
     <div className='app'>
-      <Navbar />
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
---
+++ frontend/src/components/loginPopup/loginPopup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './loginPopup.css'
+const loginPopup = () => {
+  return (
+    <div>
+      <p>login</p>
+    </div>
+  )
+}
+
+export default loginPopup
