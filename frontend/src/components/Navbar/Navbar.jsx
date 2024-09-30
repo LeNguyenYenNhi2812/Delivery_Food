d59851f88e6107acc3a885e6df18fe2ef2003098
Yen Nhi <nhi.lenguyenyennhi2004@hcmut.edu.vn>
2024-09-30 10:24:31 +0700

    cart page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom' //quen import la k chay dc Link
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 const Navbar = ({setShowLogin}) => {
     const [menu,setMenu]=useState("home");
+    const {getCartTotal}=useContext(StoreContext);
   return (
     <div className='navbar'> 
       <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
@@ -17,7 +19,7 @@ const Navbar = ({setShowLogin}) => {
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
               <Link to='/cart'> <img src={assets.basket_icon} alt="" /></Link>
-              <div className="dot"></div>
+              <div className={getCartTotal()>0 ? "dot" : ""}></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>sign in</button>
         </div>
